0917ddff5ea3f28b884282c805ea5ecc02094d11
Devang Vangane <devangvangane9@gmail.com>
2024-09-10 00:48:55 +0530

    pdf file retriever with few flaws

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from tkinter import ttk
 
 filetype_footer_header = {".png": [b"\x89\x50\x4E\x47\x0D\x0A\x1A\x0A", b"\x49\x45\x4E\x44\xAE\x42\x60\x82", "PNG"],
                           ".jpg": [b"\xff\xd8\xff\xe0\x00\x10\x4a\x46", b"\xff\xd9", "JPG"],
-                          ".pdf": ["hi", "PDF"],
+                          ".pdf": [b"%PDF-", b"%%EOF", "PDF"],
                           ".doc": [b"\xd0\xcf\x11\xe0\xa1\xb1\x1a\xe1", "DOC"]}
 
 
@@ -46,7 +46,10 @@ def recover_File():
 
     drive = f"\\\\.\\{selected_drive}"
     fileD = open(drive, "rb")
-    size = 512
+    if selected_filetype == ".pdf":
+        size = 4096
+    else:
+        size = 1024
     byte = fileD.read(size)
     offs = 0
     rcvd = 0
@@ -57,7 +60,7 @@ def recover_File():
 
     while byte:
         found = byte.find(file_header)  # PNG header
-
+        print(found)
         if found >= 0:
             drec = True
             # print(f'== Found {selected_filetype} at location: ' + str(hex(found + (size * offs))) + ' ==')
@@ -68,11 +71,10 @@ def recover_File():
                 byte = fileD.read(size)
                 bfind = byte.find(file_footer)
                 if bfind >= 0:
-                    fileN.write(byte[:bfind + 2])
+                    fileN.write(byte[:bfind + len(file_footer)])
                     fileD.seek((offs + 1) * size)
                     # print(f'== Wrote {selected_filetype} to location: ' + os.path.join(directory, str(rcvd) + fileDropdown.get()) + ' ==\n')
 
-                    # text_area.insert(tk.END, f'== Wrote {selected_filetype} to location: ' + os.path.join(directory, str(rcvd) + fileDropdown.get()) + ' ==\n')
                     drec = False
                     rcvd += 1
                     fileN.close()
@@ -86,11 +88,11 @@ def recover_File():
 
 root = tk.Tk()
 root.title("Data Recovery Application")
-root.geometry("480x460")
+root.geometry("500x340")
 
 headLabel = tk.Label(root,
                      text="Data Recovery Application",
-                     font=("Arial", 25, "bold"),
+                     font=("Arial", 28, "bold"),
                      pady=10)
 headLabel.pack()
 
@@ -100,32 +102,36 @@ style.configure("TCombobox", padding=(20, 10, 20, 10), font=("Arial", 14))
 osOptions = ["Windows", "Linux"]
 osystem = ttk.Combobox(root, values=osOptions)
 osystem.set("Select Operating System")
-osystem.place(x=20, y=70)
+osystem.place(x=30, y=70)
 osystem.config(width=30)
 osystem.config(style="TCombobox")
 osystem.bind("<<ComboboxSelected>>", update_drives)
 
 driveDropdown = ttk.Combobox(root)
 driveDropdown.set("Select Available Drive to Scan")
-driveDropdown.place(x=20, y=130)
+driveDropdown.place(x=30, y=130)
 driveDropdown.config(width=30)
 driveDropdown.config(style="TCombobox")
 
 fileTypes = [".png", ".jpg", ".pdf", ".doc", ".txt", ".docx"]
 fileDropdown = ttk.Combobox(root, values=fileTypes)
 fileDropdown.set("Select File Type")
-fileDropdown.place(x=20, y=190)
+fileDropdown.place(x=30, y=190)
 fileDropdown.config(width=30)
 fileDropdown.config(style="TCombobox")
 
 recoverButton = tk.Button(root,
-                          font=("Arial", 13, "bold"),
+                          font=("Arial", 15, "bold"),
                           foreground="black",
                           background="#cfd1d1",
                           relief="flat",
                           cursor="hand2",
                           command=recover_File)
 recoverButton.config(text=f"Recover files")
-recoverButton.place(x=20, y=250)
+recoverButton.place(x=40, y=250)
 
 root.mainloop()
+
+# coins - 2.47MB
+# boats - 4.05MB
+# pdf 7.62MB
