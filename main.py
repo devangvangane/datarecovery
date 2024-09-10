ed95161047d514c334c2f26e8b1e86f462d67964
Devang Vangane <devangvangane9@gmail.com>
2024-09-10 18:51:53 +0530

    docx working but still issues with doc and txt

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import os
 import tkinter as tk
 import psutil
 from tkinter import ttk
+import string
 
 filetype_footer_header = {".png": [b"\x89\x50\x4E\x47\x0D\x0A\x1A\x0A", b"\x49\x45\x4E\x44\xAE\x42\x60\x82", "PNG"],
                           ".jpg": [b"\xff\xd8\xff\xe0\x00\x10\x4a\x46", b"\xff\xd9", "JPG"],
                           ".pdf": [b"%PDF-", b"%%EOF", "PDF"],
-                          ".doc": [b"\xd0\xcf\x11\xe0\xa1\xb1\x1a\xe1", "DOC"]}
+                          ".doc": [b"\xd0\xcf\x11\xe0\xa1\xb1\x1a\xe1", None, "DOC"],
+                          ".docx": [b"PK\x03\x04", None, "DOCX"],
+                          ".txt": [None, None, "TXT"]}
 
 
 def list_drives_windows():
@@ -38,16 +41,25 @@ def update_drives(event):
     driveDropdown.set("Select Available Drive to Scan")
 
 
+def is_printable(byte_seq):
+    return all(0x20 <= b <= 0x7E for b in byte_seq)
+    # printable = set(bytes(string.printable, 'ascii'))  # ASCII encoded printable characters
+    # return all(b in printable for b in byte_seq)
+
+
 def recover_File():
     selected_drive = driveDropdown.get()
     selected_filetype = fileDropdown.get()
-    file_header = filetype_footer_header[selected_filetype][0]
-    file_footer = filetype_footer_header[selected_filetype][1]
+    file_header = None
+    file_footer = None
+    if selected_filetype != ".txt":
+        file_header = filetype_footer_header[selected_filetype][0]
+        file_footer = filetype_footer_header[selected_filetype][1]
 
     drive = f"\\\\.\\{selected_drive}"
     fileD = open(drive, "rb")
-    if selected_filetype == ".pdf":
-        size = 4096
+    if selected_filetype in [".pdf", ".txt"]:
+        size = 16384
     else:
         size = 1024
     byte = fileD.read(size)
@@ -59,27 +71,69 @@ def recover_File():
         os.makedirs(directory)
 
     while byte:
-        found = byte.find(file_header)  # PNG header
-        print(found)
-        if found >= 0:
-            drec = True
-            # print(f'== Found {selected_filetype} at location: ' + str(hex(found + (size * offs))) + ' ==')
-
-            fileN = open(os.path.join(directory, str(rcvd) + selected_filetype), "wb")
-            fileN.write(byte[found:])
-            while drec:
-                byte = fileD.read(size)
-                bfind = byte.find(file_footer)
-                if bfind >= 0:
-                    fileN.write(byte[:bfind + len(file_footer)])
-                    fileD.seek((offs + 1) * size)
-                    # print(f'== Wrote {selected_filetype} to location: ' + os.path.join(directory, str(rcvd) + fileDropdown.get()) + ' ==\n')
-
-                    drec = False
-                    rcvd += 1
-                    fileN.close()
-                else:
+        if selected_filetype == ".txt" and is_printable(byte):
+            print("entered if block")
+            with open(os.path.join(directory, str(rcvd) + ".txt"), "wb") as fileN:
+                print("Opened file")
+                fileN.write(byte)
+                while True:
+                    byte = fileD.read(size)
+                    print("read file")
+                    if not byte or not is_printable(byte):
+                        break
                     fileN.write(byte)
+                    print("writing file")
+            rcvd += 1
+            continue
+
+        if selected_filetype == ".doc":
+            found = byte.find(file_header)
+            if found >= 0:
+                with open(os.path.join(directory, f"doc_file_{rcvd}.doc"), "wb") as fileN:
+                    fileN.write(byte[found:])
+                    while True:
+                        byte = fileD.read(size)
+                        if not byte:
+                            break
+                        fileN.write(byte)
+                rcvd += 1
+                continue
+
+        if selected_filetype == ".docx":
+            if byte.startswith(file_header):
+                with open(os.path.join(directory, f"docx_file_{rcvd}.docx"), "wb") as fileN:
+                    fileN.write(byte)
+                    while True:
+                        byte = fileD.read(size)
+                        if not byte:
+                            break
+                        fileN.write(byte)
+                rcvd += 1
+                continue
+
+        if selected_filetype not in [".txt", ".doc", ".docx"]:
+
+            found = byte.find(file_header)  # PNG header
+            print(found)
+            if found >= 0:
+                drec = True
+                # print(f'== Found {selected_filetype} at location: ' + str(hex(found + (size * offs))) + ' ==')
+
+                fileN = open(os.path.join(directory, str(rcvd) + selected_filetype), "wb")
+                fileN.write(byte[found:])
+                while drec:
+                    byte = fileD.read(size)
+                    bfind = byte.find(file_footer)
+                    if bfind >= 0:
+                        fileN.write(byte[:bfind + len(file_footer)])
+                        fileD.seek((offs + 1) * size)
+                        # print(f'== Wrote {selected_filetype} to location: ' + os.path.join(directory, str(rcvd) + fileDropdown.get()) + ' ==\n')
+
+                        drec = False
+                        rcvd += 1
+                        fileN.close()
+                    else:
+                        fileN.write(byte)
         byte = fileD.read(size)
         offs += 1
 
@@ -134,4 +188,4 @@ root.mainloop()
 
 # coins - 2.47MB
 # boats - 4.05MB
-# pdf 7.62MB
+# pdf 37.9kb
