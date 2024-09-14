6ad0d7472a72fc8eb85971b1b70d1af2e3a28dec
Devang Vangane <devangvangane9@gmail.com>
2024-09-14 10:04:45 +0530

    ppt and pptx file recovery

--- main.py
+++ main.py
@@ -2,14 +2,29 @@ import os
 import tkinter as tk
 import psutil
 from tkinter import ttk
-import string
+import threading
+import hashlib
 
 filetype_footer_header = {".png": [b"\x89\x50\x4E\x47\x0D\x0A\x1A\x0A", b"\x49\x45\x4E\x44\xAE\x42\x60\x82", "PNG"],
                           ".jpg": [b"\xff\xd8\xff\xe0\x00\x10\x4a\x46", b"\xff\xd9", "JPG"],
                           ".pdf": [b"%PDF-", b"%%EOF", "PDF"],
                           ".doc": [b"\xd0\xcf\x11\xe0\xa1\xb1\x1a\xe1", None, "DOC"],
                           ".docx": [b"PK\x03\x04", None, "DOCX"],
+                          ".ppt": [b"\xd0\xcf\x11\xe0\xa1\xb1\x1a\xe1", None, "PPT"],
+                          ".pptx": [b"\x50\x4B\03\x04", b"\x50\x4B\x05\x06", "PPTX"],
                           ".txt": [None, None, "TXT"]}
+
+
+def start_recovery():
+    # Create a new thread for the recover_File function
+    recovery_thread = threading.Thread(target=recover_File)
+    recovery_thread.start()
+
+
+def calculate_hash(byte_data):
+    hash_md5 = hashlib.md5()
+    hash_md5.update(byte_data)
+    return hash_md5.hexdigest()
 
 
 def list_drives_windows():
@@ -86,6 +101,23 @@ def recover_File():
             rcvd += 1
             continue
 
+        if selected_filetype == ".ppt":
+            found = byte.find(file_header)  # PPT header
+            if found >= 0:
+                # print(f"Found PPT header at offset: {found + offs * size}")
+                with open(os.path.join(directory, f"ppt_file_{rcvd}.ppt"), "wb") as fileN:
+                    fileN.write(byte[found:])
+                    while True:
+                        byte = fileD.read(size)
+                        if not byte:
+                            break
+                        # Continue reading until the end of the file
+                        fileN.write(byte)
+                rcvd += 1
+                byte = fileD.read(size)
+                offs += 1
+                continue
+
         if selected_filetype == ".doc":
             found = byte.find(file_header)
             if found >= 0:
@@ -111,10 +143,10 @@ def recover_File():
                 rcvd += 1
                 continue
 
-        if selected_filetype not in [".txt", ".doc", ".docx"]:
+        if selected_filetype not in [".txt", ".doc", ".docx", ".ppt"]:
 
             found = byte.find(file_header)  # PNG header
-            print(found)
+            # print(found)
             if found >= 0:
                 drec = True
                 # print(f'== Found {selected_filetype} at location: ' + str(hex(found + (size * offs))) + ' ==')
@@ -167,7 +199,7 @@ driveDropdown.place(x=30, y=130)
 driveDropdown.config(width=30)
 driveDropdown.config(style="TCombobox")
 
-fileTypes = [".png", ".jpg", ".pdf", ".doc", ".txt", ".docx"]
+fileTypes = [".png", ".jpg", ".pdf", ".doc", ".txt", ".docx", ".ppt", ".pptx"]
 fileDropdown = ttk.Combobox(root, values=fileTypes)
 fileDropdown.set("Select File Type")
 fileDropdown.place(x=30, y=190)
@@ -180,7 +212,7 @@ recoverButton = tk.Button(root,
                           background="#cfd1d1",
                           relief="flat",
                           cursor="hand2",
-                          command=recover_File)
+                          command=start_recovery)
 recoverButton.config(text=f"Recover files")
 recoverButton.place(x=40, y=250)
 
